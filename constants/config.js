56658df58eb7622e572c79423254a9b44ec9652c
Arihant-2004 <arihanthirawat4@gmail.com>
2024-07-08 17:15:19 +0530

    new

--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,8 @@
 const corsOptions = {
     origin: [
       "https://chatapp-frontend-1.vercel.app",
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "http://localhost:4173"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
